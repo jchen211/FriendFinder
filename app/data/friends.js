f8fcd66b6fe743c736e0f5bc375a052756205663
Jessica Chen <jchen@jchen.attlocal.net>
2019-03-09 18:15:54 -0600

    gitignore added, added express, added 1st data, added get friends route

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+    {
+        friendName: "Axel", 
+        photo: "https://data.whicdn.com/images/222748035/original.gif",
+        score: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+
+        ]
+    }
+];
+
+module.exports = friends;
