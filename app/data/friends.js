88ec3e1cfcc11179b699b8c79674cd636ccb5555
Jessica Chen <jchen@jchen.attlocal.net>
2019-03-11 14:38:42 -0500

    modal appears

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,9 +13,39 @@ var friends = [
             "3",
             "4",
             "5"
-
         ]
+    } , {
+        friendName: "Benji", 
+        photo: "https://unsplash.com/photos/gJu_6pEKL24",
+        score: [
+            "3",
+            "4",
+            "1",
+            "5",
+            "1",
+            "4",
+            "1",
+            "3",
+            "3",
+            "4"
+        ]        
+    } , {
+        friendName: "Lea", 
+        photo: "https://unsplash.com/photos/gJu_6pEKL24",
+        score: [
+            "5",
+            "1",
+            "2",
+            "3",
+            "3",
+            "4",
+            "1",
+            "5",
+            "3",
+            "2"
+        ]        
     }
 ];
 
 module.exports = friends;
+
