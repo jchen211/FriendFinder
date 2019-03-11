02bc8fa85122e50282729fecd6aaf6160af79ccf
Jessica Chen <jchen@jchen.attlocal.net>
2019-03-11 15:06:43 -0500

    deploy to heorku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,4 @@
+var path = require("path");
 var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
@@ -6,7 +7,29 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        //post data from the survey
+     
+
+
+        var newFriendInfo = req.body;
+        var newFriendScore = newFriendInfo.score;
+          
+        var matchedName = "";
+        var matchedPhoto = "";
+        var totalDiff = 50;
+
+        for (var i = 0; i < friendsData.length; i++) {
+
+            var diff = 0;
+
+            for (var j =0; j < newFriendScore.length; j++) {
+                diff += Math.abs(friendsData[i].scores[j] - newFriendInfo[j]);
+            }
+                if (diff < totalDiff) {
+                    totalDiff = diff;
+                    matchedName = friendsData[i].name;
+                    matchedPhoto = friendsData[i].photo;
+                }
+            }
         friendsData.push(req.body);
         res.json(true);
     });
